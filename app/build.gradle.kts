2a9c5f73b73a2dd0f7c5e6a3ac86c8126f83e2ff
hishow1996 <125007127+hishow1996@users.noreply.github.com>
2026-09-26 13:29:28 +0800

    启用 Media3 HLS 与 DASH 播放模块

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,8 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.media3.exoplayer)
   implementation(libs.media3.datasource)
+  implementation(libs.media3.exoplayer.hls)
+  implementation(libs.media3.exoplayer.dash)
   // implementation(libs.androidx.datastore.preferences)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
